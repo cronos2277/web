983aa134f3ebddb25208451e8dbfbce56f8e1dd8
JP Windows <cronos2277@gmail.com>
2020-07-09 18:12:35 -0300

    iniciando REGEX

---
+++ REGEX/02 - BarraInvertida-Pipe-Ponto.js
@@ -0,0 +1,45 @@
+const texto = "0.1.2.3.4.5.6.7.8.9.0,1,2,3,4,5,6,7,8,9,0";
+/*
+    \ => Esse eh o caracter de escape, ele serve quando voce precisa
+    procurar por um caracter na string, porem a mesma se trata de 
+    um meta caracter e tem uma funcao dentro do Regex, nesse caso
+    o ponto eh um meta-caracter, logo precisa colocar um /\./ ao
+    inves de /./, se quiser usar o ponto ou qualquer meta caracter
+    deve usar o escape, caso nao queira ativar a funcao dele.
+    No caso a funcao vai separar a string em cada elemento do array,
+    usando o . como separador, no caso aonde tem ponto ele separa
+    o elemento em um elemento do array, repare que os numeros separados
+    por virgula estaram todos juntos no ultimo elemento do array
+*/
+console.log("Separando com base no Ponto: '/\./'");
+const regex1 = new RegExp(/\./);
+console.table(texto.split(regex1));
+
+/*
+    | => Esse caracter, seria o PIPe, ele serve como "ou" jno Regex,
+    no exemplo abaixo, toda vez que encontrar um ponto ou uma virgula
+    o numero (nesse exemplo) sera colocado em um novo espaco no array.
+*/
+console.log("Usando o PIPE: '/\.|,/'")
+const regex2 = /\.|,/;
+console.table(texto.split(regex2));
+
+/*
+    O ponto serve como caracter coringa, no caso a expressao regular abaixo
+    indica que se tiver um e apenas 1 caracter entre o numero 1 e 2, o
+    mesmo deve ser retornado. Seria algo se tiver comecando com 1 e terminando
+    com 2, e se tiver um e apenas um caracter, independente de qual seja o caracter, 
+    entao retorna o valor, repare que sera dado match em "1.2" e "1,2"
+*/
+console.log("Usando o Ponto: '/1.2/'")
+const regex3 = /1.2/g;
+console.table(texto.match(regex3));
+
+/*
+    Aqui estamos usando o metacaracter ponto '.' para interagir com o literal ponto '\.'
+    Na expressao abaixo, estamos procurando por qualquer ocorrencia de um caracter que
+    antece um ponto.
+*/
+console.log("Usando o meta-caractere ponto com o literal Ponto: '/.\\./'");
+const regex4 = /.\./g;
+console.table(texto.match(regex4));
---
+++ REGEX/readme.md
@@ -0,0 +1,13 @@
+# REGEX
+## Flags
+`/EXPRESSAO/g` => procura de maneira global, caso seja omitido apenas retorna o primeiro resultado.
+
+`/EXPRESSAO/i` => Ignora maiúsculo e minúsculo e trata ambos como iguais, no caso faz uma verificação case-insensitive.
+
+## Meta-caracteres 
+
+`\`=> metacaracter de escape.
+
+`|` => metacaracter de "ou".
+
+`.` => Substitui 1 caracter, ex: /1.2/ => nesse caso ele substitui qualquer caracter que esteja entre 1 e 2, porem caso a String seja "12" ou "1,,2" o mesmo deve dar falso, pois so deve estar apenas 1 caracter entre o 1 e o 2, nesse caso, sendo verdadeiro: "1,2", "1.2", "1e2", "132", etc...
